8f30463668bb093b3e017db028e6b6b420bafed9
Travis Hathaway <travis.j.hathaway@gmail.com>
2023-07-21 16:30:11 +0200

    version bump

--- guard/__version__.py
+++ guard/__version__.py
@@ -1,5 +1,3 @@
-__title__ = "conda_envlock"
-__description__ = (
-    "Lock and unlock conda environments to avoid mistakenly modifying them."
-)
-__version__ = "0.2.0"
+__title__ = "guard"
+__description__ = "Guard conda environments to avoid mistakenly modifying them."
+__version__ = "0.3.0"
